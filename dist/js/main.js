4bc2040d14c6830d09b9337d8568f4c6c7a474da
Dnny <mcmillandanny@gmail.com>
2019-08-15 12:37:17 -0400

    updated nav bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,11 +5,13 @@
 var hamburger = document.querySelector('.hamburger');
 var lines = document.querySelectorAll('.lines');
 var nav = document.querySelector("nav");
+var overlayNav = document.querySelector(".overlay-wrap");
 
 function hamburgerToggle() {
     lines.forEach(function (line) {
         line.classList.toggle("change");
         nav.classList.toggle("toggle-nav");
+        overlayNav.classList.toggle("opacity-overlay");
     });
 };
 hamburger.addEventListener("click", hamburgerToggle);
--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,14 @@
 const hamburger = document.querySelector('.hamburger');
 const lines = document.querySelectorAll('.lines');
 const nav = document.querySelector("nav");
+const overlayNav = document.querySelector(".overlay-wrap");
 
 
 function hamburgerToggle() {
     lines.forEach(function(line) {
         line.classList.toggle("change"); 
         nav.classList.toggle("toggle-nav"); 
+        overlayNav.classList.toggle("opacity-overlay")
     });
 };
 hamburger.addEventListener("click", hamburgerToggle);
